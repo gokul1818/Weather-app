4bdc7c6724184363bd2d63fb309096caefdd7781
gokul1818 <gokulakrishnan6978@gmail.com>
2023-03-07 12:39:50 +0530

    up

--- src/components/Weather/search.js
+++ src/components/Weather/search.js
@@ -7,6 +7,7 @@ function SearchBar({ setQuery, units, setUnits }) {
   const data = require("../../pages/city.json");
 
   const [city, setCity] = useState("");
+
   // const [value, setvalue] = useState("");
   // const [units, setUnits] = useState("");
   const onchange = (e) => {
@@ -14,8 +15,11 @@ function SearchBar({ setQuery, units, setUnits }) {
   };
 
   const onsearch = (searchterm) => {
-    setCity(searchterm);
-    console.log("search", searchterm);
+    setQuery({ q: searchterm });
+    setCity("");
+ 
+    // setCity(searchterm);
+    // alert(searchterm);
   };
   const handleUnitsChange = (e) => {
     const selectedUnit = e.currentTarget.name;
@@ -23,8 +27,10 @@ function SearchBar({ setQuery, units, setUnits }) {
   };
   const handleSearchClick = () => {
     event.preventDefault();
-    if (city !== "") setQuery({ q: city });
+
+     setQuery({ q: city });
     setCity("");
+    return;
   };
 
   const handleLocationClick = () => {
@@ -43,68 +49,69 @@ function SearchBar({ setQuery, units, setUnits }) {
   return (
     <div className={classes.content}>
       <div className={classes.sear}>
-        
-          <div className={classes.searchBar}>
-            <UilSearch
-              className={classes.icon}
-              size={15}
-              onClick={handleSearchClick}
-            />
-            {console.log(city,'city')}
-            <form onClick={handleSearchClick} >            <input
-              value={city}
-              
-              onChange={onchange}
-              type="text"
-              placeholder="Search for city...."
-              className={classes.Search}
-            />
-            </form>
+        {/* <form onClick={handleSearchClick}> */}
+        <div className={classes.searchBar}>
+          <UilSearch
+            className={classes.icon}
+            size={15}
+            onClick={handleSearchClick}
+          />
+          {/* {console.log(city, "city")} */}
+          <input
+            value={city}
+            onChange={onchange}
+            type="text"
+            // onKeyPress={handleKeypress}
+            placeholder="Search for city...."
+            className={classes.Search}
+          />
 
-            <UilLocationPinAlt
-              className={classes.icon}
-              onClick={handleLocationClick}
-              size={15}
-            />
-          </div>
-          <div className={classes
-          .drop}>
-            {data
-              .filter((item) => {
-                const searchterm = city.toLowerCase();
-                const name = item.name.toLowerCase();
-                return (
-                  searchterm &&
-                  name.startsWith(searchterm) &&
-                  name !== searchterm
-                );
-              })
-              .slice(0, 8)
-              .map((item, index) => (
-                <div className={classes.droplist} key={index} onClick={() => onsearch(item.name)}>
-                  {item.name}
-                </div>
-              ))}
-          </div>
+          <UilLocationPinAlt
+            className={classes.icon}
+            onClick={handleLocationClick}
+            size={15}
+          />
+        </div>
+        {/* </form> */}
+        <div className={classes.drop}>
+          {data
+            .filter((item) => {
+              const searchterm = city.toLowerCase();
+              const name = item.name.toLowerCase();
+              return (
+                searchterm && name.startsWith(searchterm) && name !== searchterm
+              );
+            })
+            .slice(0, 8)
+            .map((item, index) => (
+              <div
+                className={classes.droplist}
+                key={index}
+                onClick={() => onsearch(item.name)}
+              >
+                {item.name}
+              </div>
+            ))}
+        </div>
 
-          <div className={classes.unit}>
-            <button
-              name="metric"
-              onClick={handleUnitsChange}
-              className={classes.metric}
-            >
-              celsius
-            </button>
-            <p className={classes.or}> or </p>
-            <button
-              name="imperial"
-              onClick={handleUnitsChange}
-              className={classes.imperial}
-            >
-              Fahrenheit
-            </button>
-          </div>
-        
+        <div className={classes.unit}>
+          {/* {city} */}
+          <button
+            name="metric"
+            onClick={handleUnitsChange}
+            className={classes.metric}
+          >
+            celsius
+          </button>
+          <p className={classes.or}> or </p>
+          <button
+            name="imperial"
+            onClick={handleUnitsChange}
+            className={classes.imperial}
+          >
+            Fahrenheit
+          </button>
+        </div>
       </div>
     </div>
   );
